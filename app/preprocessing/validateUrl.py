a2d33a2cdc777152f3403789ca37396bc2586d70
John Lowen David <jdavi977@mtroyal.ca>
2026-07-27 12:23:40 -0600

    fix(scope): removed dead modules

--- app/preprocessing/validateUrl.py
+++
@@ -1,8 +0,0 @@
-from app.config.regex import YOUTUBE_REGEX, APP_STORE_REGEX
-
-def validateYoutube(url: str):
-    return YOUTUBE_REGEX.match(url) is not None
-    
-def validateAppStore(url: str):
-    return APP_STORE_REGEX.match(url) is not None
-
